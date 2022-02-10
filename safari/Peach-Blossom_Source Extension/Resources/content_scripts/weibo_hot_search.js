308eac6603aaccf9b4ec4a6fc948ac42ce31c1da
mondo <helloxiangsheng@163.com>
2022-02-10 16:01:34 +0800

    .

--- safari/Peach-Blossom_Source Extension/Resources/content_scripts/weibo_hot_search.js
+++ safari/Peach-Blossom_Source Extension/Resources/content_scripts/weibo_hot_search.js
@@ -1,36 +1,83 @@
-let filterKeyword = [
-    "党",
-    "总书记",
-    "习近平",
-    "中共中央",
-    "人民群众",
-    "始终坚持以",
-    "发展思想",
-    "冬奥会",
-    
-    "俄罗斯",
-    "美国",
-    "乌克兰"
-];
-
-let removeCartItem = [];
-let cartItemList = document.querySelectorAll('.vue-recycle-scroller__item-wrapper > .vue-recycle-scroller__item-view');
-
-cartItemList.forEach(item =>  {
-
-    let cartTitle  = item.querySelector('h2');
-    if(cartTitle != null){
-        const result =  filterKeyword.filter(keyword => cartTitle.innerText.indexOf(keyword) !== -1);
-        console.log(result);
-        if(result != null && result.length > 0){
-            removeCartItem.push(item);
+// 获取本地数据
+function loadLocalData(){
+    browser.storage.local.get('filter_keyword').then(items => {
+        if(JSON.stringify(items) !== "{}"){
+            filterHotContent(items.filter_keyword.split(','));
         }
+        console.log(items);
+    }, error => console.log(error));
+}
+
+
+const keywords = ['羽生结弦','美国'];
+
+// 微博内容为动态生成
+let intervalID = setInterval(function(){
+    let item_wrapper = document.querySelector('.vue-recycle-scroller__item-wrapper');
+    if(item_wrapper !== null){
+        clearInterval(intervalID);
+        
+        //filterHotContent(keywords, item_wrapper);
+        
+        //registerObserver();
+    }
+}, 100);
+
+
+// 过滤热榜内容
+function filterHotContent(filterKeyword, target){
+    let cartItemList = target.querySelectorAll('.vue-recycle-scroller__item-view');
+    
+    cartItemList.forEach(item =>  {
+        let cartTitle  = item.querySelector('.HotTopic_tit_eS4fv');
+        
+        if(cartTitle != null){
+            // 如果包含过滤关键字，返回数组
+            const result = filterKeyword.filter(keyword => {
+                if(keyword.indexOf("/") === -1){ // 判断是否为正则
+                    return cartTitle.innerText.indexOf(keyword) !== -1;
+                }
+                
+                // 正则过滤
+                return new RegExp(keyword.replaceAll("/","")).test(cartTitle.innerText);
+            });
+            
+            if(Array.isArray(result) && result.length > 0){
+                if(item.style.display !== 'none'){
+                    item.style.display = 'none';
+                }
+                
+            } else {
+                if(item.style.display !== 'block'){
+                    item.style.display = 'block';
+                }
+            }
+        }
+    });
+}
+
+
+// 注册观察
+function registerObserver(){
+    function callback(mutationList, observer) {
+      mutationList.forEach((mutation) => {
+        switch(mutation.type) {
+            case 'attributes':
+                filterHotContent(keywords, mutation.target);
+                console.log(mutation);
+              break;
+        }
+      });
     }
 
-});
-
-console.log(removeCartItem);
-
-while(removeCartItem.length > 0){
-    removeCartItem.pop().remove();
+    var observer = new MutationObserver(callback);
+    observer.observe(
+                     document.querySelector('.vue-recycle-scroller__item-wrapper'),
+                     {
+                        attributes: true,
+                        //attributeFilter: ["transform"],
+                        childList: true,
+                        subtree: true
+                     }
+                     );
 }
--- safari/Peach-Blossom_Source Extension/Resources/content_scripts/zhihu_hot.js
+++ safari/Peach-Blossom_Source Extension/Resources/content_scripts/zhihu_hot.js
@@ -1,7 +1,4 @@
-//browser.storage.sync.remove("filter_keyword");
-
-let cartItemList = document.querySelectorAll('#TopstoryContent section'); // item
-let removeCartItem = [];
+let cartItemList = document.querySelectorAll('#TopstoryContent section'); // 热门搜索
 
 // 获取本地数据
 function loadLocalData(){
@@ -9,13 +6,45 @@ function loadLocalData(){
         if(JSON.stringify(items) !== "{}"){
             filterHotContent(items.filter_keyword.split(','));
         }
-        
+        console.log(items);
     }, error => console.log(error));
 }
 
 // 过滤热榜内容
 function filterHotContent(filterKeyword){
+    let removeCartItem = [];
+    
     cartItemList.forEach(item =>  {
+        let cartHref  = item.querySelector('a');
+        let cartTitle  = item.querySelector('h2');
+        
+        if(cartTitle != null){
+            const result = filterKeyword.filter(keyword => {
+                if(keyword.indexOf("/") === -1){ // 判断是否为正则
+                    return cartTitle.innerText.indexOf(keyword) !== -1;
+                }
+                
+                // 正则过滤
+                return new RegExp(keyword.replaceAll("/","")).test(cartTitle.innerText);
+            });
+            
+            if(Array.isArray(result) && result.length > 0){
+                removeCartItem.push(item);
+            }
+        }
+    });
+
+    while(removeCartItem.length > 0){
+        removeCartItem.pop().remove();
+    }
+}
+
+// 过滤推荐内容
+function filterContent(filterKeyword){
+    let removeCartItem = [];
+    let cartItemRecommendList = document.querySelectorAll('.Topstory-recommend .TopstoryItem'); // 推荐条目
+    
+    cartItemRecommendList.forEach(item =>  {
         let cartTitle  = item.querySelector('h2');
         if(cartTitle != null){
             const result = filterKeyword.filter(keyword => cartTitle.innerText.indexOf(keyword) !== -1);
@@ -40,7 +69,7 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             } else {
                 let keyword_array = items.filter_keyword.split(',');
                 keyword_array.push(request.message);
-                items = keyword_array.join(','); // 应该去重复
+                items = [...new Set(keyword_array)].join(',');
             }
             
             browser.storage.local.set({"filter_keyword": items});
@@ -54,4 +83,42 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 
 loadLocalData();
-console.log('zhihu hot');
+
+
+// 监听导航改变
+function callback(mutationList, observer) {
+  mutationList.forEach((mutation) => {
+    switch(mutation.type) {
+      case 'childList':
+            console.log('child_list', mutation);
+            
+            let listShortcut = document.querySelector('#TopstoryContent > .ListShortcut');
+            let hotList = listShortcut.querySelector('.HotList');
+            let topstory_recommend = listShortcut.querySelector('.Topstory-recommend');
+            let topstory_follow = listShortcut.querySelector('.Topstory-follow');
+            
+            if(mutation.addedNodes.length > 0){
+                if(hotList !== null ){
+                    console.log('hotList');
+                    
+                } else if(topstory_recommend !== null){
+                    console.log('topstory_recommend');
+                    
+                } else if(topstory_follow !== null){
+                    console.log('topstory_follow');
+                }
+            }
+        
+        break;
+    }
+  });
+}
+
+//var observer = new MutationObserver(callback);
+//observer.observe(document.querySelector('#TopstoryContent'), { attributes: true, childList: true, subtree: false });
+
+
+// 监听子条目增减, 只过滤单个内容
+//var observer = new MutationObserver(callback);
+//observer.observe(document.querySelector('#TopstoryContent > .ListShortcut > .Topstory-recommend > div'),
+//                 { attributes: true, childList: true, subtree: false });
